cceda2409cebf5146fcea984ed54a7d1cbea9a8f
Matt Krueger <mkrueger@rstms.net>
2025-06-26 12:37:58 -0600

    Bump version: 0.0.6 → 0.0.7

--- cmd/root.go
+++ cmd/root.go
@@ -39,7 +39,7 @@ import (
 var cfgFile string
 
 var rootCmd = &cobra.Command{
-	Version: "0.0.6",
+	Version: "0.0.7",
 	Use:     "tarsnap-maildir-restore",
 	Short:   "restore tarsnap maildir backup",
 	Long: `
